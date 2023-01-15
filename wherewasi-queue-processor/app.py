25defc7e2a7d54acca369237022443cfa318285d
theemadnes <alex.mattson@gmail.com>
2023-01-15 03:58:46 +0000

    commits to DB appear correct

--- wherewasi-queue-processor/app.py
+++ wherewasi-queue-processor/app.py
@@ -19,7 +19,7 @@ if os.getenv("TIMEOUT", None):
     print(f'Running with timeout of {timeout} seconds.')
 else:
     timeout = None
-    print("Running with no timeout. This will clear the queue but won't write to the database.")
+    print("Running with no timeout.")
 max_messages = int(os.getenv("MAX_MESSAGES", 10))
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
@@ -31,28 +31,38 @@ database = instance.database(os.getenv("DATABASE_ID"))
 table = os.getenv("TABLE_ID")
 print(f"Spanner instance: {instance.name}\nSpanner database: {database.name}\nSpanner table: {table}")
 
-# use connection API to get existing records
-connection = connect(os.getenv("INSTANCE_ID"), os.getenv("DATABASE_ID"))
-connection.autocommit = True
-
-cursor = connection.cursor()
-cursor.execute(f"SELECT * FROM {table}")
-
-current_data = cursor.fetchall()
-current_data_dict = dict(current_data) #list_to_dict(current_data)
-
-updates = {}
-new_data = {}
+# variable to track how many messages have been processed so far; limited by $CHECKPOINT_MESSAGE_COUNT
+messages_processed = 0 
+max_messages_processed = int(os.getenv("CHECKPOINT_MESSAGE_COUNT", 500))
 
 def insert_zone_metrics(transaction):
 
-    for k in current_data_dict.keys():
-        if k in zone_count.keys():
-            updates[k] = current_data_dict[k] + zone_count[k]
+    # stage zone counts
+    global zone_count # laziness
+    zone_count_stage = zone_count.copy()
+    zone_count = defaultdict(lambda: 0) # reset zone count
 
-    for j in zone_count.keys():
+    # use connection API to get existing records
+    connection = connect(os.getenv("INSTANCE_ID"), os.getenv("DATABASE_ID"))
+    connection.autocommit = True
+
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table}")
+
+    current_data = cursor.fetchall()
+    current_data_dict = dict(current_data) #list_to_dict(current_data)
+
+    # dicts used to determine whether to insert or update records in DB
+    updates = {}
+    new_data = {}
+
+    for k in current_data_dict.keys():
+        if k in zone_count_stage.keys():
+            updates[k] = current_data_dict[k] + zone_count_stage[k]
+
+    for j in zone_count_stage.keys():
         if j not in current_data_dict.keys():
-            new_data[j] = zone_count[j]
+            new_data[j] = zone_count_stage[j]
 
     if len(new_data):
     
@@ -73,14 +83,22 @@ def insert_zone_metrics(transaction):
         print("{} record(s) updated.".format(len(updates)))
 
 zone_count = defaultdict(lambda: 0) # tracks counts per zone
-executor = ThreadPoolExecutor(10)
+executor = ThreadPoolExecutor(10) # handles message acks async
 
 def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    global messages_processed # being lazy
     print(f"Received {message.data!r}.")
     zone_count[json.loads(message.data.decode())['zone']] += 1
     print(dict(zone_count))
     #message.ack()
+    messages_processed += 1
     future = executor.submit(message.ack(), ("Completed")) # send `ack` to threadpool executor
+    # test to see if we've hit $CHECKPOINT_MESSAGE_COUNT; if, so, write to DB
+    print(messages_processed)
+    if messages_processed >= max_messages_processed:
+        print(f"Hit max messaged processed value of {max_messages_processed}")
+        messages_processed = 0
+        database.run_in_transaction(insert_zone_metrics)
 
 # Limit the subscriber to only have ten outstanding messages at a time.
 flow_control = pubsub_v1.types.FlowControl(max_messages=max_messages)
